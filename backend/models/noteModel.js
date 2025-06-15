0c69fcfefc08f9ba47e80c7f890447c02c0d9468
aokiji <rcm.webdev.pro@gmail.com>
2025-06-15 11:53:16 -0700

    create a note model with a specific schema

---
+++ backend/models/noteModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+}, { timestamps: true });
+
+//create a note model with this schema
+const Note = mongoose.model("Note", noteSchema);
+module.exports = Note;
