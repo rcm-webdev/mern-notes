8f873115bf47d5635483c460d04baf4d726cddd1
aokiji <rcm.webdev.pro@gmail.com>
2025-06-14 17:21:16 -0700

    install nodemon, dotenv, express and setup backend to prepare for routes

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 
+
 //dotenv is a module that loads env variables from the .env file in the config directory
 dotenv.config({path: "./config/.env"});
 
-app.get("/api/notes", (req, res) => {
-    res.send("You have created an api endpoint")
-})
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}. You better go catch it!`);
