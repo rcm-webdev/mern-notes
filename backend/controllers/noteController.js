32d300a0257e6a9f53d1b084860ca94b74cdc473
aokiji <rcm.webdev.pro@gmail.com>
2025-06-15 11:55:10 -0700

    setup controller logic to handle CRUD operations

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,18 +1,53 @@
+const Note = require('../models/noteModel')
 
-const getNotes = (req, res) => {
-    res.send("this is an api endpoint from the notes route, and the logic is in the noteController")   
+const getNotes = async (req, res) => {
+    try {
+        const notes = await Note.find()
+        res.status(200).json(notes)
+    } catch (err) {
+        console.log(`Error getting notes: ${err.message}`)
+        res.status(500).json({message: err.message})
+    }
 }
 
-const createNote = (req, res) => {
-    res.json({message: `You have created a post with id: ${req.params.id}`})
+//HTTP POST request to create a new note
+const createNote = async (req, res) => {
+    
+    try {
+        const {title, content} = req.body
+        if (!title || !content) {
+            return res.status(400).json({message: 'Title and content required'})
+        }
+        const newNote = await Note.create({title, content})
+        res.status(201).json({message: `Note ${newNote._id} created successfully`})
+    } catch (err) {
+        console.log(`Error creating note: ${err.message}`)
+        res.status(500).json({message: err.message})
+    }
 }
 
-const updateNote = (req, res) => {
-    res.json({message: `You have updated note ${req.params.id}`})
+//HTTP PUT request to update a note
+const updateNote = async(req, res) => {
+   try{
+    const {id} = req.params
+    const {title, content} = req.body
+    const updatedNote = await Note.findByIdAndUpdate(id, {title, content}, {new: true})
+    res.status(200).json({message: `Note ${id} updated successfully`})
+   } catch (err) {
+    console.log(`Error updating note: ${err.message}`)
+}
 }
 
-const deleteNote = (req, res) => {
-    res.json({message: `You have deleted note ${req.params.id}`})
+
+//HTTP DELETE request to delete a note
+const deleteNote = async(req, res) => {
+    try{
+        const {id} = req.params
+        await Note.findByIdAndDelete(id)
+        res.status(200).json({message: `Note ${id} deleted successfully`})
+    } catch (err) {
+        console.log(`Error deleting note: ${err.message}`)
+    }
 }
 
 module.exports = {
