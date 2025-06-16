aa6cd3e06c39b81f8b640f8b541d56b26fe4cca5
aokiji <rcm.webdev.pro@gmail.com>
2025-06-16 10:22:23 -0700

    styling: remove data-theme attribute and add gradient snippet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,17 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="min-h-screen bg-base-300" data-theme="forest">
-      
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateNote />} />
-        <Route path="/note/:id" element={<Note />} />
-      </Routes>
+    <div className="relative min-h-screen">
+      {/* Background gradient: need to change with the color of the theme */}
+      <div className="fixed inset-0 -z-10 h-full w-full [background:radial-gradient(125%_150%_at_50%_10%,#000_40%,#21B855_100%)]"></div>
+      <div className="relative">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateNote />} />
+          <Route path="/note/:id" element={<Note />} />
+        </Routes>
+      </div>
     </div>
   )
 }
