b00ff761a209861ae39f8e1867c26d14db3ce497
aokiji <rcm.webdev.pro@gmail.com>
2025-06-16 08:33:49 -0700

    styling: add no notes found component when there are no notes

---
+++ frontend/src/components/NoNotesFound.jsx
@@ -0,0 +1,23 @@
+import { NotebookIcon, PlusIcon } from "lucide-react"
+import { Link } from "react-router"
+
+const NoNotesFound = () => {
+    return(
+        <div className="flex flex-col items-center justify-center py-16 space-y-6 max-w-md mx-auto text-center">
+            <div className="bg-primary/10 rounded-full p-4">
+                <NotebookIcon className="size-16 text-primary"/>
+            </div>
+            <div>
+                <h2 className="text-2xl font-bold">No notes found</h2>
+                <p className="text-base-content/50">Create a new note to get started</p>
+            </div>
+            <div>
+                <Link to="/create" className="btn btn-primary">
+                <PlusIcon className="size-4 mr-2"/>
+                Create Your First Note</Link>
+            </div>
+        </div>
+    )
+}
+
+export default NoNotesFound;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import api from "../lib/axios";
 import NoteCard from "../components/NoteCard";
 import { useState, useEffect } from "react";
+import NoNotesFound from "../components/NoNotesFound";
 
 const Home = () => {
     const [notes, setNotes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const fetchNotes = async () => {
         try {
@@ -11,6 +13,8 @@ const Home = () => {
             setNotes(res.data);
         } catch (error) {
             console.error("Error fetching notes:", error);
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -22,16 +26,29 @@ const Home = () => {
         setNotes(notes.filter(note => note._id !== deletedNoteId));
     };
 
+    if (isLoading) {
+        return (
+            <div className="max-w-4xl mx-auto p-4 flex justify-center items-center min-h-[200px]">
+                <span className="loading loading-spinner text-primary"></span>
+            </div>
+        );
+    }
+
     return (
         <div className="max-w-4xl mx-auto p-4">
+            {notes.length === 0 && (
+                <NoNotesFound/>
+            )}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {notes.map((note) => (
-                <NoteCard 
-                    key={note._id} 
-                    note={note} 
-                    onDelete={handleNoteDelete}
-                />
-              ))}
+                {notes.length > 0 && (
+                    notes.map((note) => (
+                        <NoteCard 
+                            key={note._id} 
+                            note={note} 
+                            onDelete={handleNoteDelete}
+                        />
+                    ))
+                )}
             </div>
         </div>
     )
