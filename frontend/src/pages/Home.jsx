9fbb9da50f1f07e552d0489f873d583fba336bdd
aokiji <rcm.webdev.pro@gmail.com>
2025-06-15 23:27:43 -0700

    views: handle rendering page after deleted items

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,25 +5,32 @@ import { useState, useEffect } from "react";
 const Home = () => {
     const [notes, setNotes] = useState([]);
 
-   useEffect(() => {
     const fetchNotes = async () => {
         try {
-           
             const res = await axios.get("http://localhost:2121/api/notes");
-            console.log("fetched data")
             setNotes(res.data);
         } catch (error) {
             console.error("Error fetching notes:", error);
         }
     }
-    fetchNotes();
-  }, [])
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    const handleNoteDelete = (deletedNoteId) => {
+        setNotes(notes.filter(note => note._id !== deletedNoteId));
+    };
 
     return (
         <div className="max-w-4xl mx-auto p-4">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {notes.map((note) => (
-                <NoteCard key={note._id} note={note} />
+                <NoteCard 
+                    key={note._id} 
+                    note={note} 
+                    onDelete={handleNoteDelete}
+                />
               ))}
             </div>
         </div>
